443dccbfa1fcc5e34159d036cbb3b8937c220f9c
Luis Felipe Diaz Valbuena <>
2025-02-26 21:37:06 -0500

    Create new product initial commit

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,31 @@
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
 namespace Catalog.API.Products.CreateProduct;
 
-public class CreateProductHandler
+public record CreateProductCommand(
+    string Name,
+    string Description,
+    List<string> Category,
+    string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Business logic to create a product
+        var product = new Product
+        {
+            Name = command.Name,
+            Description = command.Description,
+            Category = command.Category,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+
+// Add services to the container.
+builder.Services.AddCarter(new DependencyContextAssemblyCatalog([assembly]));
+builder.Services.AddMediatR(configuration => { configuration.RegisterServicesFromAssembly(assembly); });
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
